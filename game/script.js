2acb47cdf4edee692245806b29c80e626e0a74d1
Momo <mobuttondev@gmail.com>
2023-01-19 01:13:29 -0500

    score counter

--- game/script.js
+++ game/script.js
@@ -1,22 +1,13 @@
 'use strict';
-/*console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number';
-console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value = 23;*/
 
 const number = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = number;
-console.log(number);
+let score = 20;
+
 
 document.querySelector('.check').addEventListener('click', function(){
 	const guess = Number (document.querySelector('.guess').value);
-	console.log(typeof guess);
-
 
 	if(!guess){
 		document.querySelector('.message').textContent = 'no number!';
@@ -24,8 +15,12 @@ document.querySelector('.check').addEventListener('click', function(){
 		document.querySelector('.message').textContent = 'Correct';
 	}else if(guess > number){
 		document.querySelector('.message').textContent = 'Number is too high';
+		score --;
+		document.querySelector('.score').textContent = score;
 	}else if(guess < number){
 		document.querySelector('.message').textContent = 'Number is too low';
+		score --;
+		document.querySelector('.score').textContent = score;
 	}
 
 })
