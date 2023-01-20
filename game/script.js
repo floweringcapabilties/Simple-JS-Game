71367f84d87765b6386ccf4f6f83af51e3dbd799
Momo <mobuttondev@gmail.com>
2023-01-19 22:39:10 -0500

    logic

--- game/script.js
+++ game/script.js
@@ -3,37 +3,38 @@
 
 const number = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = number;
-let score = 3;
+let score = 20;
 
+
+function getMessage(mssg){
+	document.querySelector('.message').textContent = mssg;
+}
+function getScore(x){
+	document.querySelector('.score').textContent = x;
+}
 
 document.querySelector('.check').addEventListener('click', function(){
 	const guess = Number (document.querySelector('.guess').value);
 
 	if(!guess){
-		document.querySelector('.message').textContent = 'no number!';
+		getMessage('no number!');
 	}else if (guess === number){
-		document.querySelector('.message').textContent = 'Correct';
-	}else if(guess > number){
-		if(score > 1){
-			document.querySelector('.message').textContent = 'Number is too high';
-			score --;
-			document.querySelector('.score').textContent = score;
-		}else{
-			score --;
-			document.querySelector('.score').textContent = score;
-			document.querySelector('.message').textContent = 'You Lose';
-		}
-	
-	}else if(guess < number){
-		if(score > 1){
-			document.querySelector('.message').textContent = 'Number is too high';
-			score --;
-			document.querySelector('.score').textContent = score;
-		}else{
-			score --;
-			document.querySelector('.score').textContent = score;
-			document.querySelector('.message').textContent = 'You Lose';
-		}
+		getMessage('Correct');
+		document.querySelector('body').style.backgroundColor = '#60b347';
+	}
+ 
+	if(guess > number && score >= 1){
+		getMessage('Number is too high');
+		score --;
+		getScore(score);
+	}else if(guess < number && score >= 1){
+		getMessage('Number is too low');
+		score --;
+		getScore(score);
+	}
+
+	if(score == 0){
+		getMessage('You Lose');
 	}
 
 })
