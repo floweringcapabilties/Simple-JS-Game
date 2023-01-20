9445225783a880775dee1299002b3df7847e8d45
Momo <mobuttondev@gmail.com>
2023-01-19 23:10:26 -0500

    logic & refactor

--- game/script.js
+++ game/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
+let number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = '?';
 let score = 20;
 
 
@@ -12,15 +12,25 @@ function getMessage(mssg){
 function getScore(x){
 	document.querySelector('.score').textContent = x;
 }
+function playAgain(){
+	number = Math.trunc(Math.random() * 20) + 1;
+	score = 20;
+	document.querySelector('.number').textContent = '?';
+	document.querySelector('body').style.backgroundColor = '#222';
+	document.querySelector('.guess').value = '';
+	getMessage('Start guessing...');
+	getScore(score);
+}
 
 document.querySelector('.check').addEventListener('click', function(){
 	const guess = Number (document.querySelector('.guess').value);
-
 	if(!guess){
 		getMessage('no number!');
 	}else if (guess === number){
 		getMessage('Correct');
+		document.querySelector('.number').textContent = number;
 		document.querySelector('body').style.backgroundColor = '#60b347';
+		document.querySelector('.number').style.width = '30rem';
 	}
  
 	if(guess > number && score >= 1){
@@ -34,7 +44,12 @@ document.querySelector('.check').addEventListener('click', function(){
 	}
 
 	if(score == 0){
+		document.querySelector('.number').textContent = number;
 		getMessage('You Lose');
 	}
 
+})
+
+document.querySelector('.again').addEventListener('click', function(){
+	playAgain();
 })
